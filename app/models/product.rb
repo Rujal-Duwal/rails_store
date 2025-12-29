b38fe9cc97d0754fc1cb474ee4dff80cb92e3b47
Rujal-Duwal <czar69@live.com>
2025-12-29 21:13:31 +0545

    Add inventory management and subscription feature for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 # app/models/product.rb
 class Product < ApplicationRecord
+  has_many :subscribers, dependent: :destroy
   has_one_attached :featured_image
   has_rich_text :description
+
   validates :name, presence: true
+  validates :inventory_count, numericality: { greater_than_or_equal_to: 0 }
 end
